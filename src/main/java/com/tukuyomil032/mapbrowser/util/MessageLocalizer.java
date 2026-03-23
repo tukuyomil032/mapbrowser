3a0a684e7b5547efb1cfb6c6d3ca9fe8c50ddea8
tukuyomil032 <yinkom0708@gmail.com>
2026-03-23 16:20:23 +0900

    refactor: split localization catalogs by domain
    add dedicated input message yaml files
    
    load multi-catalog resources in MessageLocalizer

--- src/main/java/com/tukuyomil032/mapbrowser/util/MessageLocalizer.java
+++ src/main/java/com/tukuyomil032/mapbrowser/util/MessageLocalizer.java
@@ -30,7 +30,9 @@ public final class MessageLocalizer {
         this.enPrefix = new LinkedHashMap<>();
         this.jaPrefix = new LinkedHashMap<>();
         load(plugin, "messages_en.yml", enExact, enPrefix);
+        load(plugin, "messages_input_en.yml", enExact, enPrefix);
         load(plugin, "messages_ja.yml", jaExact, jaPrefix);
+        load(plugin, "messages_input_ja.yml", jaExact, jaPrefix);
     }
 
     /**
